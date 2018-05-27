dcab744c4c09e8a9ad45663a9f489031603946e7
Sabri Eyuboglu <sabrieyuboglu@DN51utdf.SUNet>
2018-05-27 16:43:51 -0700

    Added recall output during GCN training

--- source/method_gcn.py
+++ source/method_gcn.py
@@ -37,7 +37,7 @@ def compute_gcn_scores(ppi_adj_sparse, features_sparse, train_pos, val_pos, para
 
     # Run training 
     data = format_data(features_sparse, Y, ppi_adj_sparse, train_nodes, val_nodes)
-    epoch_outputs = perform_train(*data, params = params, verbose=True)
+    epoch_outputs = perform_train(*data, train_pos = train_pos, params = params, verbose=True)
     scores = epoch_outputs[-1][:,1]
 
     return scores
